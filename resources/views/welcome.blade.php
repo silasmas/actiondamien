750d68d412d6929e7aae698bc8aa21560d7e1f5a
xanderssamoth <bolulathomas@yahoo.fr>
2022-10-27 09:07:19 +0100

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -343,14 +343,38 @@
         </div>
         <!-- Report End -->
 
-        <!-- News Start -->
-        <div class="container-xxl py-5 bg-green">
+
+        <!-- Testimonial Start -->
+        <div class="container-xxl py-5">
             <div class="container">
-                <div class="row">
-                    <div class="col-12 p-5"></div>
+                <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 500px;">
+                    <div class="d-inline-block rounded-pill bg-secondary text-primary py-1 px-3 mb-3">Actualité</div>
+                    <h1 class="display-6 mb-5">Lisez nos derniers articles et conseils.</h1>
+                </div>
+
+                <div class="owl-carousel testimonial-carousel wow fadeInUp" data-wow-delay="0.1s">
+                    <div class="testimonial-item text-center">
+                        <img class="img-fluid bg-light rounded-circle p-2 mx-auto mb-4" src="img/testimonial-1.jpg" style="width: 100px; height: 100px;">
+
+                        <div class="testimonial-text rounded text-center p-4">
+                            <p>Clita clita tempor justo dolor ipsum amet kasd amet duo .</p>
+                            <h5 class="mb-1">Doner Name</h5>
+                            <span class="fst-italic">Profession</span>
+                        </div>
+                    </div>
+
+                    <div class="testimonial-item text-center">
+                        <img class="img-fluid bg-light rounded-circle p-2 mx-auto mb-4" src="img/testimonial-2.jpg" style="width: 100px; height: 100px;">
+
+                        <div class="testimonial-text rounded text-center p-4">
+                            <p>Clita clita tempor justo dolor ipsum amet kasd amet duo justo duo duo labore sed sed.</p>
+                            <h5 class="mb-1">Doner Name</h5>
+                            <span class="fst-italic">Profession</span>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-        <!-- News End -->
+        <!-- Testimonial End -->
 
 @endsection
