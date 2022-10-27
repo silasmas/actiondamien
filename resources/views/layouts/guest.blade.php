908979340b1c132b491537aab63d79b2c98398ca
xanderssamoth <bolulathomas@yahoo.fr>
2022-10-27 04:59:03 +0100

    Update guest.blade.php

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -94,7 +94,9 @@
             <nav class="navbar navbar-expand-lg navbar-light py-lg-0 px-lg-5 wow fadeIn" data-wow-delay="0.1s">
                 <a href="" class="navbar-brand ms-4 ms-lg-0">
                     <img src="{{ asset('assets/img/logo-01.png') }}" alt="Action Damien" class="" width="150">
+                </a>
 
+                <div class="navbar-toggler me-4">
                     <span class="d-lg-none d-inline-block">
                         <!-- Language toggle -->
 @foreach ($available_locales as $locale_name => $available_locale)
@@ -114,11 +116,11 @@
     @endif
 @endforeach
                     </span>
-                </a>
 
-                <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+                    <button type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                </div>
 
                 <div class="collapse navbar-collapse" id="navbarCollapse">
                     <div class="navbar-nav ms-auto p-4 p-lg-0 text-uppercase">
