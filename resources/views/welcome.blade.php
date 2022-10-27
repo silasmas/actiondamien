8a795a84949fb792b43c8998b4a26df19bcc0e9f
xanderssamoth <bolulathomas@yahoo.fr>
2022-10-27 09:16:31 +0100

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -354,7 +354,7 @@
 
                 <div class="owl-carousel testimonial-carousel wow fadeInUp" data-wow-delay="0.1s">
                     <div class="testimonial-item text-center">
-                        <img class="img-fluid bg-light rounded-circle p-2 mx-auto mb-4" src="img/testimonial-1.jpg" style="width: 100px; height: 100px;">
+                        <img class="img-fluid bg-light p-2 mx-auto mb-4" width="50" src="{{ asset('assets/img/photo/ANNEXE 11 IMAGE POUR LA PARTIE NOUS SOUTENIR.jpeg') }}">
 
                         <div class="testimonial-text rounded text-center p-4">
                             <p>Clita clita tempor justo dolor ipsum amet kasd amet duo .</p>
@@ -364,7 +364,7 @@
                     </div>
 
                     <div class="testimonial-item text-center">
-                        <img class="img-fluid bg-light rounded-circle p-2 mx-auto mb-4" src="img/testimonial-2.jpg" style="width: 100px; height: 100px;">
+                        <img class="img-fluid bg-light p-2 mx-auto mb-4" width="50" src="{{ asset('assets/img/photo/ANNEXE 11 IMAGE POUR LA PARTIE NOUS SOUTENIR.jpeg') }}">
 
                         <div class="testimonial-text rounded text-center p-4">
                             <p>Clita clita tempor justo dolor ipsum amet kasd amet duo justo duo duo labore sed sed.</p>
