c96fcafa56c55184566ebfce8b3dffb7c9078b84
xanderssamoth <bolulathomas@yahoo.fr>
2022-10-28 15:55:58 +0100

    Update guest.blade.php

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -66,7 +66,7 @@
 
                 <div class="col-lg-8 px-5 text-end" style="background-color: #fdd68f; padding-top: 0.7rem; padding-bottom: 0.8rem;">
                     <!-- Social networks -->
-                    <small ><a class="me-3" style="color:#58b8a7 !important"  href="#"><i class="fa fa-envelope me-2 align-middle"></i>secretaire@actiondamien-rdc.net</a></small>
+                    <small ><a class="me-3 text-white"  href="#"><i class="fa fa-envelope me-2 align-middle"></i>secretaire@actiondamien-rdc.net</a></small>
                     <a class="text-dark ms-2" href="#"><i class="fab fa-facebook-f"></i></a>
                     <a class="text-dark ms-3" href="#"><i class="fab fa-twitter"></i></a>
                     <a class="text-dark ms-3 me-5" href="#"><i class="fab fa-instagram"></i></a>
