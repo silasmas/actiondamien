97db4adef45a029fd2342ca30336c9f865ab9a6f
xanderssamoth <bolulathomas@yahoo.fr>
2022-11-03 04:11:15 +0100

    c

---
+++ resources/views/hosp_map.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.guest')
+
+@section('guest-content')
+        <!-- Page Header Start -->
+        <div class="container-fluid page-header mb-5 wow fadeIn" data-wow-delay="0.1s">
+            <div class="container text-center">
+                <h1 class="display-4 text-white animated slideInDown mb-4">404 Error</h1>
+                <nav aria-label="breadcrumb animated slideInDown">
+                    <ol class="breadcrumb justify-content-center mb-0">
+                        <li class="breadcrumb-item"><a class="text-white" href="#">Home</a></li>
+                        <li class="breadcrumb-item"><a class="text-white" href="#">Pages</a></li>
+                        <li class="breadcrumb-item text-primary active" aria-current="page">404 Error</li>
+                    </ol>
+                </nav>
+            </div>
+        </div>
+        <!-- Page Header End -->
+
+        <!-- About Start -->
+        <div class="container-xxl py-5">
+            <div class="container">
+            </div>
+        </div>
+        <!-- About End -->
+
+@endsection
