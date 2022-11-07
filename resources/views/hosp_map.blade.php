a7a153598f7352de79ff51b2a848b9d2c6aa3e8f
xanderssamoth <bolulathomas@yahoo.fr>
2022-11-07 15:25:10 +0100

    Update hosp_map.blade.php

--- resources/views/hosp_map.blade.php
+++ resources/views/hosp_map.blade.php
@@ -1,15 +1,15 @@
 @extends('layouts.guest')
 
 @section('guest-content')
+
         <!-- Page Header Start -->
-        <div class="container-fluid page-header mb-5 wow fadeIn" data-wow-delay="0.1s">
+        <div class="container-fluid page-header wow fadeIn" data-wow-delay="0.1s" style="background: linear-gradient(rgba(25, 29, 35, .5), rgba(25, 29, 35, .5)), url({{ asset('assets/img/photo/ANNEXE_3_PHOTO_DU_CEDA_1.jpg') }}) center center no-repeat; background-size: cover;">
             <div class="container text-center">
-                <h1 class="display-4 text-white animated slideInDown mb-4">404 Error</h1>
+                <h1 class="display-4 mb-4 text-white animated slideInDown">{{ __('miscellaneous.hospitals.link2') }}</h1>
                 <nav aria-label="breadcrumb animated slideInDown">
                     <ol class="breadcrumb justify-content-center mb-0">
-                        <li class="breadcrumb-item"><a class="text-white" href="#">Home</a></li>
-                        <li class="breadcrumb-item"><a class="text-white" href="#">Pages</a></li>
-                        <li class="breadcrumb-item text-primary active" aria-current="page">404 Error</li>
+                        <li class="breadcrumb-item"><a class="text-white" href="./">{{ __('miscellaneous.main_menu.home') }}</a></li>
+                        <li class="breadcrumb-item text-yellow active" aria-current="page">{{ __('miscellaneous.hospitals.link2') }}</li>
                     </ol>
                 </nav>
             </div>
