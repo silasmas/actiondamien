5db2e77be00b6d6ba370b8a608474d381e40a41b
xanderssamoth <bolulathomas@yahoo.fr>
2022-11-15 14:01:12 +0100

    Update hosp_map.blade.php

--- resources/views/hosp_map.blade.php
+++ resources/views/hosp_map.blade.php
@@ -23,8 +23,8 @@
                     <div class="col-12 wow fadeInUp" data-wow-delay="0.1s">
                         <div class="input-group">
                             <input type="text" class="form-control" aria-label="{{ __('miscellaneous.inner_page.hospitals.search') }}" placeholder="{{ __('miscellaneous.inner_page.hospitals.search') }}" style="border-start-start-radius: 0.7rem; border-end-start-radius: 0.7rem;">
-                            <span class="input-group-text p-0 border-blue overflow-hidden" style="border-end-end-radius: 0.7rem; border-start-end-radius: 0.7rem;">
-                                <button class="btn btn-blue px-4 rounded-0" style="height: 60px;"><i class="fa fa-search"></i></button>
+                            <span class="input-group-text p-0 border-0 overflow-hidden" style="border-end-end-radius: 0.7rem; border-start-end-radius: 0.7rem;">
+                                <button class="btn btn-green px-4 rounded-0" style="height: 60px;"><i class="fa fa-search"></i></button>
                             </span>
                         </div>
                     </div>
@@ -34,16 +34,77 @@
         <!-- Search info End -->
 
         <!-- Hospitals list Start -->
-        <div class="container-xxl py-5">
+        <div class="container-xxl pb-5">
             <div class="container">
-                <div class="row g-4">
-                    <div class="col-lg-4 col-sm-6">
-                        <div class="card border border-default shadow-0">
-                            <div class="card-body text-center">
-                                <i class="fa fa-hospital display-4"></i>
-                                <h1 class="h1 m-0">BANDALUNGWA</h1>
+                <div class="row g-5">
+                    <div class="col-lg-6 col-sm-12">
+                        <div class="card border border-default overflow-hidden shadow-0">
+                            <div class="card-header bg-light">
+                                <h3 class="h3 m-0 fw-normal">Bandalungwa</h3>
                                 <a href="#" class="stretched-link bg-danger"></a>
                             </div>
+                            <ul class="list-group list-group-flush rounded-0">
+                                <li class="list-group-item px-4 py-3">
+                                    <h5 class="h5 mb-1 fw-bold text-green">LIBIKISI</h5>
+                                    <h6 class="h6 mb-3">Bisengo</h6>
+                                    <p class="mb-1 fw-light">
+                                        <i class="fa fa-map-marker-alt me-2"></i>Av. Kasa-Vubu N°87/BIS, Q/Bisengo, C/Bandalungwa
+                                    </p>
+                                    <p class="m-0 fw-light">
+                                        <i class="fa fa-phone me-2"></i>(+243) 998 189 705
+                                    </p>
+                                </li>
+                                <li class="list-group-item px-4 py-3">
+                                    <h5 class="h5 mb-1 fw-bold text-green">RIVIERA CLINIC</h5>
+                                    <h6 class="h6 mb-3">Kasa-Vubu</h6>
+                                    <p class="mb-1 fw-light">
+                                        <i class="fa fa-map-marker-alt me-2"></i>Av. Nsenge N°5116, Q/Bakayau, C/Bandalungwa
+                                    </p>
+                                    <p class="m-0 fw-light">
+                                        <i class="fa fa-phone me-2"></i>(+243) 998 579 586
+                                    </p>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-sm-12">
+                        <div class="card border border-default overflow-hidden shadow-0">
+                            <div class="card-header bg-light">
+                                <h3 class="h3 m-0 fw-normal">Barumbu</h3>
+                                <a href="#" class="stretched-link bg-danger"></a>
+                            </div>
+                            <ul class="list-group list-group-flush rounded-0">
+                                <li class="list-group-item px-4 py-3">
+                                    <h5 class="h5 mb-1 fw-bold text-green">BOYAMBI</h5>
+                                    <h6 class="h6 mb-3">Bitshakutshaku</h6>
+                                    <p class="mb-1 fw-light">
+                                        <i class="fa fa-map-marker-alt me-2"></i>Av. Kasai N°12, Q/Armée du Salut, C/Barumbu
+                                    </p>
+                                    <p class="m-0 fw-light">
+                                        <i class="fa fa-phone me-2"></i>(+243) 998 908 513
+                                    </p>
+                                </li>
+                                <li class="list-group-item px-4 py-3">
+                                    <h5 class="h5 mb-1 fw-bold text-green">MATERNITE KASAI</h5>
+                                    <h6 class="h6 mb-3">Kasai</h6>
+                                    <p class="mb-1 fw-light">
+                                        <i class="fa fa-map-marker-alt me-2"></i>C/Barumbu
+                                    </p>
+                                    <p class="m-0 fw-light">
+                                        <i class="fa fa-phone me-2"></i>
+                                    </p>
+                                </li>
+                                <li class="list-group-item px-4 py-3">
+                                    <h5 class="h5 mb-1 fw-bold text-green">BRALIMA</h5>
+                                    <h6 class="h6 mb-3">Ndolo</h6>
+                                    <p class="mb-1 fw-light">
+                                        <i class="fa fa-map-marker-alt me-2"></i>Av. Flambeau N° , C/Barumbu
+                                    </p>
+                                    <p class="m-0 fw-light">
+                                        <i class="fa fa-phone me-2"></i>(+243) 851 109 360 / 818 288 808
+                                    </p>
+                                </li>
+                            </ul>
                         </div>
                     </div>
                 </div>
