4d16cf58394a32f9c4c17f252bf2353e5105a82a
xanderssamoth <bolulathomas@yahoo.fr>
2022-12-05 15:58:39 +0100

    Update hosp_map.blade.php

--- resources/views/hosp_map.blade.php
+++ resources/views/hosp_map.blade.php
@@ -37,12 +37,14 @@
         <div class="container-xxl py-3 bg-light">
             <div class="container">
                 <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 600px;">
-                    <h3 class="h3-6 mb-5">{{ __('miscellaneous.inner_page.hospitals.legend.title') }}</h3>
+                    <h3 class="h3 mb-5">{{ __('miscellaneous.inner_page.hospitals.legend.title') }}</h3>
                 </div>
 
                 <div class="row text-center align-items-center">
                     <div class="col-12 wow fadeInUp" data-wow-delay="0.3s">
-
+                        <span class="d-inline-block me-4 mb-4"><i class="fa fa-circle fs-4 text-danger me-2"></i>{{ __('miscellaneous.inner_page.hospitals.legend.data1') }}</span>
+                        <span class="d-inline-block me-4 mb-4"><i class="fa fa-circle fs-4 text-success me-2"></i>{{ __('miscellaneous.inner_page.hospitals.legend.data2') }}</span>
+                        <span class="d-inline-block me-4 mb-4"><i class="fa fa-circle fs-4 text-blue me-2"></i>{{ __('miscellaneous.inner_page.hospitals.legend.data3') }}</span>
                     </div>
                 </div>
             </div>
