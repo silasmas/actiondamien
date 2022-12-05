55840f02cc5f9f033ce0dfd2379449e5f2944d4c
xanderssamoth <bolulathomas@yahoo.fr>
2022-12-05 15:50:00 +0100

    Update hosp_map.blade.php

--- resources/views/hosp_map.blade.php
+++ resources/views/hosp_map.blade.php
@@ -34,7 +34,7 @@
         <!-- Search info End -->
 
         <!-- Legend Start -->
-        <div class="container-xxl py-5">
+        <div class="container-xxl py-3 bg-light">
             <div class="container">
                 <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 600px;">
                     <h1 class="display-6 mb-5">{{ __('miscellaneous.inner_page.hospitals.legend.title') }}</h1>
