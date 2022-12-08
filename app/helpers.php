096339251fe4e2b0b470632d4337b79dd3c36016
silasmas <silasjmas@gmail.com>
2022-12-08 15:48:55 +0100

    ok

--- app/helpers.php
+++ app/helpers.php
@@ -3,11 +3,7 @@
 if (!function_exists("formatNumber")) {
     function formatNumber($number)
     {
-        $lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
-        $acceptLang = ['ln', 'fr', 'en'];
-        $lang = in_array($lang, $acceptLang) ? $lang : 'en';
-
-        if ($lang == 'en') {
+        if (app()->getLocale() !== 'en') {
             return number_format($number, 0, ',', ' ');
 
         } else {
