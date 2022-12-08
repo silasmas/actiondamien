94462feb4ba2e86fb7f58c77b6cfd5de6d0a7ba5
xanderssamoth <bolulathomas@yahoo.fr>
2022-12-08 15:08:20 +0100

    Update helpers.php

--- app/helpers.php
+++ app/helpers.php
@@ -3,6 +3,10 @@
 if (!function_exists("formatNumber")) {
     function formatNumber($number)
     {
+        //  ========================================================
+        //  FIRST POSSIBILITY
+        //  ========================================================
+
         // $lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
         // $acceptLang = ['ln', 'fr', 'en'];
         // $lang = in_array($lang, $acceptLang) ? $lang : 'en';
@@ -14,22 +18,37 @@ if (!function_exists("formatNumber")) {
         //     return number_format($number, 0, ',', ' ');
         // }
 
-        $available_locales = config('app.available_locales');
-        $current_locale = app()->getLocale();
+        //  ========================================================
+        //  SECOND POSSIBILITY
+        //  ========================================================
 
-        foreach ($available_locales as $available_locale):
-            if ($available_locale != $current_locale) {
-                if ($available_locale == 'en') {
-                    return number_format($number, 0, '.', ',');
+        // $available_locales = config('app.available_locales');
+        // $current_locale = app()->getLocale();
 
-                } else {
-                    return number_format($number, 0, ',', ' ');
-                }
+        // foreach ($available_locales as $available_locale):
+        //     if ($available_locale === $current_locale) {
+        //         return number_format($number, 0, ',', ' ');
 
-            } else {
-                return number_format($number, 0, ',', ' ');
-            }
-        endforeach;
+        //     } else {
+        //         if ($available_locale === 'en') {
+        //             return number_format($number, 0, '.', ',');
+
+        //         } else {
+        //             return number_format($number, 0, ',', ' ');
+        //         }
+        //     }
+        // endforeach;
+
+        //  ========================================================
+        //  THIRD POSSIBILITY
+        //  ========================================================
+
+        if (app()->getLocale() !== 'en') {
+            return number_format($number, 0, ',', ' ');
+
+        } else {
+            return number_format($number, 0, '.', ',');
+        }
     }
 }
 
