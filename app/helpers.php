8ee6a03680c3bea7b09f2911d2fa43e29748fdd9
xanderssamoth <bolulathomas@yahoo.fr>
2022-12-08 14:52:23 +0100

    Update helpers.php

--- app/helpers.php
+++ app/helpers.php
@@ -3,16 +3,33 @@
 if (!function_exists("formatNumber")) {
     function formatNumber($number)
     {
-        $lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
-        $acceptLang = ['ln', 'fr', 'en'];
-        $lang = in_array($lang, $acceptLang) ? $lang : 'en';
+        // $lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+        // $acceptLang = ['ln', 'fr', 'en'];
+        // $lang = in_array($lang, $acceptLang) ? $lang : 'en';
 
-        if ($lang == 'en') {
-            return number_format($number, 0, '.', ',');
+        // if ($lang == 'en') {
+        //     return number_format($number, 0, '.', ',');
 
-        } else {
-            return number_format($number, 0, ',', ' ');
-        }
+        // } else {
+        //     return number_format($number, 0, ',', ' ');
+        // }
+
+        $available_locales = config('app.available_locales');
+        $current_locale = app()->getLocale();
+
+        foreach ($available_locales as $available_locale):
+            if ($available_locale != $current_locale) {
+                if ($available_locale == 'en') {
+                    return number_format($number, 0, '.', ',');
+
+                } else {
+                    return number_format($number, 0, ',', ' ');
+                }
+
+            } else {
+                return number_format($number, 0, ',', ' ');
+            }
+        endforeach;
     }
 }
 
