73ed6ef63215f84880d2d1a9f310a0a913cb3d96
xanderssamoth <bolulathomas@yahoo.fr>
2022-12-08 15:38:31 +0100

    Update template.blade.php

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -408,7 +408,7 @@
         </div>
 
         <nav class="navbar navbar-expand-lg navbar-light py-lg-0 px-lg-5 wow fadeIn" data-wow-delay="0.1s">
-            <a href="" class="navbar-brand ms-4 ms-lg-0">
+            <a href="{{ route('home') }}" class="navbar-brand ms-4 ms-lg-0">
                 <img src="{{ asset('assets/img/logo-01.png') }}" alt="Action Damien" class="" width="150">
             </a>
 
