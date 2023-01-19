58502be2242fc29ca3898b1f44cb605bd7f817b3
xanderssamoth <bolulathomas@yahoo.fr>
2023-01-19 13:21:05 +0100

    Update sensibilization.blade.php

--- resources/views/sensibilization.blade.php
+++ resources/views/sensibilization.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.template')
 
 @section('guest-content')
+    @if (Route::current()->getName() == 'sensibilization_details')
         <!-- Page Header Start -->
         <div class="container-fluid page-header wow fadeIn" data-wow-delay="0.1s" style="background: linear-gradient(rgba(25, 29, 35, .5), rgba(25, 29, 35, .5)), url({{ asset('assets/img/photo/1.jpg') }}) center center no-repeat; background-size: cover;">
             <div class="container text-center">
@@ -8,14 +9,14 @@
                 <nav aria-label="breadcrumb animated slideInDown">
                     <ol class="breadcrumb justify-content-center mb-0">
                         <li class="breadcrumb-item"><a class="text-white" href="./">{{ __('miscellaneous.main_menu.home') }}</a></li>
-                        <li class="breadcrumb-item text-yellow active" aria-current="page">{{ __('miscellaneous.main_menu.testimonials.sensibilization') }}</li>
+                        <li class="breadcrumb-item"><a class="text-white" href="{{ route('sensibilization') }}">{{ __('miscellaneous.main_menu.testimonials.sensibilization') }}</a></li>
+                        <li class="breadcrumb-item text-yellow active" aria-current="page">{{ __('miscellaneous.inner_page.sensibilization.title') }}</li>
                     </ol>
                 </nav>
             </div>
         </div>
         <!-- Page Header End -->
 
-    @if (Route::current()->getName() == 'sensibilization_details')
         <!-- Title Start -->
         <div class="container-xxl py-5">
             <div class="container">
@@ -105,6 +106,22 @@
         </div>
         <!-- Content End -->
     @else
+        <!-- Page Header Start -->
+        <div class="container-fluid page-header wow fadeIn" data-wow-delay="0.1s" style="background: linear-gradient(rgba(25, 29, 35, .5), rgba(25, 29, 35, .5)), url({{ asset('assets/img/photo/1.jpg') }}) center center no-repeat; background-size: cover;">
+            <div class="container text-center">
+                <h1 class="display-4 mb-4 text-white animated slideInDown">{{ __('miscellaneous.main_menu.testimonials.sensibilization') }}</h1>
+                <nav aria-label="breadcrumb animated slideInDown">
+                    <ol class="breadcrumb justify-content-center mb-0">
+                        <li class="breadcrumb-item"><a class="text-white" href="./">{{ __('miscellaneous.main_menu.home') }}</a></li>
+                        <li class="breadcrumb-item"><a class="text-white" href="{{ route('sensibilization') }}">{{ __('miscellaneous.main_menu.testimonials.sensibilization') }}</a></li>
+                        <li class="breadcrumb-item text-yellow active" aria-current="page">{{ __('miscellaneous.inner_page.sensibilization.title') }}</li>
+                    </ol>
+                </nav>
+            </div>
+        </div>
+        <!-- Page Header End -->
+
+        <!-- Sensibilizations list Start -->
         <div class="container-xxl py-5 border-top border-default">
             <div class="container py-3">
                 <div class="row">
@@ -134,6 +151,7 @@
                 </div>
             </div>
         </div>
+        <!-- Sensibilizations list End -->
     @endif
 
 @endsection
