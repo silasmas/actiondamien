c74b8d5ff145f4f45992e1aebc7e20ddff6c87ac
xanderssamoth <bolulathomas@yahoo.fr>
2023-01-19 13:49:20 +0100

    Update sensibilization.blade.php

--- resources/views/sensibilization.blade.php
+++ resources/views/sensibilization.blade.php
@@ -3,7 +3,7 @@
 @section('guest-content')
     @if (Route::current()->getName() == 'sensibilization_details')
         <!-- Page Header Start -->
-        <div class="container-fluid page-header wow fadeIn" data-wow-delay="0.1s" style="background: linear-gradient(rgba(25, 29, 35, .5), rgba(25, 29, 35, .5)), url({{ asset('assets/img/photo/1.jpg') }}) center center no-repeat; background-size: cover;">
+        <div class="container-fluid page-header wow fadeIn" data-wow-delay="0.1s" style="background: linear-gradient(rgba(25, 29, 35, .5), rgba(25, 29, 35, .5)), url({{ asset('assets/img/photo/index14.jpg') }}) center center no-repeat; background-size: cover;">
             <div class="container text-center">
                 <h1 class="display-4 mb-4 text-white animated slideInDown">{{ __('miscellaneous.main_menu.testimonials.sensibilization') }}</h1>
                 <nav aria-label="breadcrumb animated slideInDown">
