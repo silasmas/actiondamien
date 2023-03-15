2a9615d436cf731593f3f6fa868a1608b15edea1
silasmas <silasjmas@gmail.com>
2023-03-15 18:19:22 +0100

    ok

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -249,8 +249,7 @@
                     </div>
                     <div class="col-sm-6 col-12 pt-sm-0 pt-4">
                         <div class="bg-image">
-                            <img src="{{ asset('assets/img/photo/map.png') }}" alt="markers"
-                                class="img-fluid">
+                            <img src="{{ asset('assets/img/photo/map.png') }}" alt="markers" class="img-fluid">
                             <div class="mask"></div>
                         </div>
                     </div>
