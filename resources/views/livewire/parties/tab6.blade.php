eee731609401d82ef669e26997da2f572698c2c3
silasmas <silasjmas@gmail.com>
2025-04-07 10:38:39 +0100

    Update tab6.blade.php

--- resources/views/livewire/parties/tab6.blade.php
+++ resources/views/livewire/parties/tab6.blade.php
@@ -1,5 +1,5 @@
 <div class="row">
-    <div class="col-sm-6">
+    <div class="col-sm-4">
         <div class="ibox">
             <div class="ibox-content">
 
@@ -68,7 +68,7 @@
             </div>
         </div>
     </div>
-    <div class="col-sm-6">
+    <div class="col-sm-8">
         <div class="ibox ">
             <div class="ibox-content">
                 <div class="tab-content">
